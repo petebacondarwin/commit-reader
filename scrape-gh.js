4cdd00c02db1e5024403185be2385c07da46e758
Peter Bacon Darwin <pete@bacondarwin.com>
2015-10-18 20:28:45 +0100

    fix(scrape-gh): support limiting the number of pages to download

--- scrape-gh.js
+++ scrape-gh.js
@@ -7,8 +7,8 @@ var links = {};
 var args = process.argv;
 while(args[0] == 'node' || args[0] == __filename) args.shift();
 
-if (args.length !== 4) {
-  console.log('Usage: node scrape-gh.js owner repo user password');
+if (args.length !== 4 && args.length !== 5) {
+  console.log('Usage: node scrape-gh.js owner repo user password pages');
   return;
 }
 
@@ -16,6 +16,8 @@ var owner = args[0];
 var repo = args[1];
 var user = args[2];
 var password = args[3];
+var pages = args[4] ? parseInt(args[4]) : NaN;
+var page = 0;
 
 var promise = makeRequest('https://api.github.com/repos/' + owner + '/' + repo + '/commits?per_page=100');
 
@@ -26,6 +28,8 @@ promise.then(function() {
 
 function makeRequest(url) {
   console.log('request', url);
+
+  page++;
 
   return rp({
     url: url,
@@ -40,7 +44,8 @@ function makeRequest(url) {
 
     // If there is another page then get it
     var links = parseLinks(response);
-    if (links.next) {
+    if (links.next && page !== pages) {
+      console.log(page, pages);
       return makeRequest(links.next);
     }
 
